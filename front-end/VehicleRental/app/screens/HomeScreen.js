1473e32f171af1522c7af74c1ae48886eec2c9f7
IGVasilev19 <IGVasilev19@codingburgas.bg>
2023-03-31 18:17:56 +0300

    Add Button for adding a new vehicle

--- front-end/VehicleRental/app/screens/HomeScreen.js
+++ front-end/VehicleRental/app/screens/HomeScreen.js
@@ -1,6 +1,18 @@
-import { Text, StyleSheet, Dimensions, Image } from "react-native";
-import { Box, HStack, Menu, Pressable } from "native-base";
-import React, { useContext } from "react";
+import { Text, StyleSheet, Image } from "react-native";
+import {
+  Box,
+  HStack,
+  Menu,
+  Pressable,
+  Center,
+  Modal,
+  FormControl,
+  Input,
+  Button,
+  Icon,
+} from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import Tabs from "../components/Tabs";
 import { useCore } from "../providers/CoreProvider";
@@ -8,6 +20,9 @@ import { useCore } from "../providers/CoreProvider";
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { user, setUser } = useCore();
+  const [showModal, setShowModal] = useState(false);
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
 
   const onAccountPressed = () => {
     navigation.navigate("Account");
@@ -58,6 +73,67 @@ const HomeScreen = () => {
         </Menu>
       </HStack>
       <Tabs key="HomeScreen" />
+      <Center>
+        <Button
+          style={styles.addButton}
+          left={140}
+          bgColor="darkBlue.600"
+          onPress={() => setShowModal(true)}
+        >
+          <Icon color="white" as={AntDesign} name="plus" size="7" />
+        </Button>
+        <Modal
+          width="100%"
+          isOpen={showModal}
+          onClose={() => setShowModal(false)}
+        >
+          <Modal.Content
+            style={{ backgroundColor: "#363636" }}
+            maxWidth="400px"
+          >
+            <Modal.CloseButton />
+            <Modal.Header style={{ backgroundColor: "#363636" }}>
+              <Text style={{ color: "white", fontSize: 20 }}>
+                Add a new vehicle
+              </Text>
+            </Modal.Header>
+            <Modal.Body>
+              <HStack space={2}>
+                <Input
+                  placeholder="Brand"
+                  selectionColor="white"
+                  color={"white"}
+                  w={"49%"}
+                  value={brand}
+                  onChangeText={setBrand}
+                  placeholderTextColor="white"
+                />
+
+                <Input
+                  right={1}
+                  placeholder="Model"
+                  selectionColor="white"
+                  color={"white"}
+                  w={"49%"}
+                  value={model}
+                  onChangeText={setModel}
+                  placeholderTextColor="white"
+                />
+              </HStack>
+            </Modal.Body>
+            <Modal.Footer style={{ backgroundColor: "#363636" }}>
+              <Button
+                onPress={() => {
+                  setShowModal(false);
+                }}
+              >
+                Save
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+      </Center>
+      ;
     </Box>
   );
 };
@@ -78,6 +154,11 @@ const styles = StyleSheet.create({
     borderRadius: 55,
     borderColor: "white",
     borderWidth: 3,
+  },
+  addButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 55,
   },
 });
 export default HomeScreen;
